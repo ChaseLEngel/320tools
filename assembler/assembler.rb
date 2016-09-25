fcd87eec555e33b44378eb33fd168e91ca5f4636
ChaseLEngel <chaselengel@gmail.com>
2016-09-24 23:54:25 -0700

    Fixed bug with removing raw.

--- assembler/assembler.rb
+++ assembler/assembler.rb
@@ -25,5 +25,5 @@ page = Nokogiri::HTML(response)
 # Grab text from third div on page. Assumed to always be assembled instructions.
 instr_content = page.css('div#content')[2].text
 # Remove header text, leading \n\t and ending \n
-assembled = instr_content.tr('Memory initialization file contents', '').strip
+assembled = instr_content.gsub('Memory initialization file contents', '').strip
 File.new(ARGV[1], 'w').puts assembled
